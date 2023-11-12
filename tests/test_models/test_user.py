e2e5c12564fe9283ad6d8c91cf01c94da7800934
Rachid KABBAB <r.kabbab@gmail.com>
2023-11-12 10:27:20 -0800

    unit test of User class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+"""
+Unittest for the User class
+Test files by using the following command line:
+python3 -m unittest tests/test_models/test_user.py
+"""
+import unittest
+import pep8
+from os import path, remove
+import datetime
+import models
+from models import user
+from models.user import User
+from models.engine.file_storage import FileStorage
+
+
+class TestUser(unittest.TestCase):
+    """ Declares variables and methods of TestCase """
+
+    def setUp(self):
+        """
+        Assigns an empty string to public class attributes of User ""
+        Method called to prepare the test fixture.
+        It gets called immediately before calling the test method
+        """
+        User.email = ""
+        User.password = ""
+        User.first_name = ""
+        User.last_name = ""
+        FileStorage._FileStorage__objects = {}
+        FileStorage._FileStorage__file_path = "file.json"
+
+    def tearDown(self):
+        """
+        Deletes public class props from User
+        Method called immediately after the test method is called
+        """
+        del User.email
+        del User.password
+        del User.first_name
+        del User.last_name
+        del FileStorage._FileStorage__file_path
+        del FileStorage._FileStorage__objects
+        if path.exists("file.json"):
+            remove("file.json")
+
+    def test_pep8_conformance(self):
+        """ Test that User conforms to PEP8 standards """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0, "Errors (and warnings).")
+
+    def test_class_method_presence(self):
+        """ Test that the User methods are defined """
+        u = dir(User)
+        self.assertIn('__init__', u)
+        self.assertIn('to_dict', u)
+        self.assertIn('save', u)
+        self.assertIn('__str__', u)
+
+    def test_class_attribute_presence(self):
+        """ Test that the User attributes are declared """
+        u = dir(User)
+        self.assertIn('email', u)
+        self.assertIn('password', u)
+        self.assertIn('first_name', u)
+        self.assertIn('last_name', u)
+
+    def test_instance_method_presence(self):
+        """Test that the User instance has the same methods"""
+        u = dir(User())
+        self.assertIn('__init__', u)
+        self.assertIn('save', u)
+        self.assertIn('to_dict', u)
+        self.assertIn('__str__', u)
+
+    def test_instance_attribute_presence(self):
+        """Test that the User instance attributes are declared"""
+        u = dir(User())
+        self.assertIn('id', u)
+        self.assertIn('updated_at', u)
+        self.assertIn('created_at', u)
+        self.assertIn('__class__', u)
+        self.assertIn('email', u)
+        self.assertIn('password', u)
+        self.assertIn('first_name', u)
+        self.assertIn('last_name', u)
+
+    def test_docstring_presence(self):
+        """Test that docstring is present
+        in Module, Class, and methods"""
+        self.assertIsNot(user.__doc__, None)
+        self.assertIsNot(User.__doc__, None)
+        self.assertIsNot(User.__init__.__doc__, None)
+        self.assertIsNot(User.save.__doc__, None)
+        self.assertIsNot(User.to_dict.__doc__, None)
+        self.assertIsNot(User.__str__.__doc__, None)
+
+    def test_instantiation(self):
+        """Test correct instantiation of object of type User"""
+
+        u = User()
+        self.assertIsInstance(u, User)
+        self.assertIsInstance(u.id, str)
+        self.assertIsInstance(u.created_at, datetime.datetime)
+        self.assertIsInstance(u.updated_at, datetime.datetime)
+        self.assertIsInstance(u.__class__, type)
+
+        u.size = "Short"
+        lu = dir(u)
+        self.assertIn('size', lu)
+        self.assertEqual(u.__dict__['size'], 'Short')
+
+        u.size = 'Short'
+        ll = dir(u)
+        self.assertIn('size', ll)
+        self.assertEqual(u.__dict__['size'], 'Short')
+
+        u.age = 32
+        l3 = dir(u)
+        self.assertIn('age', l3)
+        self.assertEqual(u.__dict__['age'], 32)
+
+        u.age = 32.5
+        l4 = dir(u)
+        self.assertIn('age', l4)
+        self.assertEqual(u.__dict__['age'], 32.5)
+
+        u.age = None
+        l5 = dir(u)
+        self.assertIn('age', l5)
+        self.assertEqual(u.__dict__['age'], None)
+
+        us1 = User(**{})
+        self.assertIsInstance(us1, User)
+        self.assertIsInstance(us1.id, str)
+        self.assertIsInstance(us1.created_at, datetime.datetime)
+        self.assertIsInstance(us1.updated_at, datetime.datetime)
+        self.assertIsInstance(us1.__class__, type)
+
+        # us2 = User(**{"first_name": "Rachid", "age": 32})
+        # l6 = dir(us2)
+        # self.assertIn('first_name', l6)
+        # self.assertIn('age', l6)
+        # self.assertEqual(us2.__dict__['first_name'], 'Rachid')
+        # self.assertEqual(us2.__dict__['age'], 32)
+
+    def test_save(self):
+        """Test save method"""
+
+        us = User()
+        temp = us.__dict__['updated_at']
+        self.assertFalse(path.isfile('file.json'))
+        us.save()
+        self.assertTrue(path.isfile('file.json'))
+        self.assertNotEqual(us.__dict__['updated_at'], temp)
+        temp = us.__dict__['updated_at']
+        models.storage.reload()
+        self.assertEqual(us.__dict__['updated_at'], temp)
+
+    def test_to_dict(self):
+        """Test to_dict method call"""
+
+        us = User()
+        us.age = 32
+        us.size = "Short"
+        for k, v in us.__dict__.items():
+            if k != 'updated_at' and k != 'created_at':
+                self.assertIn(k, us.to_dict())
+                self.assertEqual(v, us.to_dict()[k])
+        self.assertEqual(us.to_dict()['__class__'], us.__class__.__name__)
+        self.assertEqual(us.to_dict()['updated_at'], us.updated_at.isoformat())
+        self.assertEqual(us.to_dict()['created_at'], us.created_at.isoformat())
+        self.assertEqual(us.to_dict()['age'], 32)
+        self.assertEqual(us.to_dict()['size'], 'Short')
+        self.assertIsInstance(us.to_dict(), dict)
+
+    def test_str(self):
+        """Test __str__ method"""
+
+        u = User()
+        string = '['+u.__class__.__name__+']'+' ('+u.id+') '+str(u.__dict__)
+        self.assertEqual(string, u.__str__())
